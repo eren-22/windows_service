6e5e0d2719105e3e6cc50573f52df044ce2b0ed4
eren-22 <erenozz@yahoo.com>
2024-02-21 17:05:52 +0300

    windows service
    Creating a manually operated service.

---
+++ WindowsService1/WindowsService1/Service1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer();
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            DosyaYaz("Servis çalışmaya başladı" + DateTime.Now.ToString());
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000;
+            timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            DosyaYaz("Servis durdu " + DateTime.Now.ToString());
+        }
+
+        private void OnElapsedTime(object source,ElapsedEventArgs e)
+        {
+            DosyaYaz("Servis çalışmaya devam ediyor " + DateTime.Now.ToString());
+        }
+
+        public void DosyaYaz (string mesaj)
+        {
+            string dosyaYolu = AppDomain.CurrentDomain.BaseDirectory + "/Logs";
+
+            if (!Directory.Exists(dosyaYolu))
+            {
+                Directory.CreateDirectory(dosyaYolu);
+            }
+
+            string textYolu = AppDomain.CurrentDomain.BaseDirectory + "/Logs/servisim.txt";
+
+            if (!File.Exists(textYolu))  //hata!!
+            {
+                using(StreamWriter sw = File.CreateText(textYolu))
+                {
+                    sw.WriteLine(mesaj);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(textYolu))
+                {
+                    sw.WriteLine(mesaj);
+                }
+            }
+        }
+
+        
+
+    }
+}
